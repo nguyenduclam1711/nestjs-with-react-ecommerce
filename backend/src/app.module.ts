46226557f333711665f0ebd63d9fd01d61ba09a5
Lam LUL <nguyenduclam1711@gmail.com>
2024-11-14 16:30:31 +0700

    Add prisma with postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, PrismaService, UserService],
 })
 export class AppModule {}
